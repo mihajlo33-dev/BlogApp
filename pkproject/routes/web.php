f920141bda8d6537d23ec7fdc86b10814e231eba
mihajlo33-dev <mdrljaca@rocektmail.com>
2021-04-04 19:05:09 +0200

    app push

---
+++ pkproject/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/','App\Http\Controllers\PagesController@index');
+Route::get('/about','App\Http\Controllers\PagesController@about');
+Route::get('/services','App\Http\Controllers\PagesController@services');
+
+// Route::get('/hello', function () {
+//     return '<h1>Hello World</h1>';
+// });
+//
+// Route::get('/about', function () {
+//     return view('pages.about');
+// });
+
+Route::get('/users/{id}', function ($id) {
+    return 'This is a user ' . $id;
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('posts', 'App\Http\Controllers\PostsController');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
